5a50924f6febab8b5a979918824b24ee884bf853
Jonathan <jadougdev@gmail.com>
2025-03-12 07:00:14 -0500

    added profile testing

--- src/test/java/com/credibanco/transactions/infraestructure/repository/TransactionJpaRepositoryTest.java
+++ src/test/java/com/credibanco/transactions/infraestructure/repository/TransactionJpaRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -23,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class TransactionJpaRepositoryTest {
 
     @Autowired
